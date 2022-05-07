48bc8aaaa96220e0f612e2436ed3fc43cd86483c
mh-bigdeli <103386851+mh-bigdeli@users.noreply.github.com>
2022-05-07 18:11:22 -0400

    Support the network

--- internal/participate/sidebars.ts
+++ internal/participate/sidebars.ts
@@ -1,18 +1,15 @@
 module.exports = {
   participate: [
-    'support-the-network/about-nodes',
-    'support-the-network/become-a-node-operator',
-    'support-the-network/node-software',
-    // {
-    //   type: 'category',
-    //   label: 'Support The Network',
-    //   collapsed: false,
-    //   items: [
-    //     'support-the-network/about-nodes',
-    //     'support-the-network/become-a-node-operator',
-    //     'support-the-network/node-software',
-    //   ],
-    // },
+    {
+      type: 'category',
+      label: 'Support The Network',
+      collapsed: false,
+      items: [
+        'support-the-network/about-nodes',
+        'support-the-network/become-a-node-operator',
+        'support-the-network/node-software',
+      ],
+    },
     // {
     //   type: 'category',
     //   label: 'The Community',
